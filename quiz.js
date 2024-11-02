eba1321c19394387d8322c326f9502e48b7da767
GustavoTerres <104083216+GustavoTerres@users.noreply.github.com>
2024-11-02 19:38:41 -0300

    .

---
+++ quiz.js
@@ -0,0 +1,21 @@
+
+const quizContainer = document.getElementById("quiz-container");
+
+const perguntas = [
+    {
+        pergunta: "O que significa IA?",
+        opcoes: ["Inteligência Artificial", "Interatividade Assistida", "Interface de Análise"],
+        respostaCorreta: "Inteligência Artificial"
+    },
+    {
+        pergunta: "Qual linguagem é mais usada para IA?",
+        opcoes: ["Python", "HTML", "CSS"],
+        respostaCorreta: "Python"
+    },
+    {
+        pergunta: "Qual desses é um exemplo de IA usada em casa?",
+        opcoes: ["Assistentes virtuais como Alexa e Google Assistant", "Calculadora", "Rádio"],
+        respostaCorreta: "Assistentes virtuais como Alexa e Google Assistant"
+    },
+];
+
